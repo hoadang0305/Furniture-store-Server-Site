023dbb3264c97d9a207ce274cf7f9c5c17e4c635
Hòa Đặng <dangnhathoa22@gmail.com>
2023-11-04 19:47:29 +0700

    update product models

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -6,19 +6,7 @@ const getAllProducts = require('../controller/products.js');
 //CREATE - only for admin
 router.post('/newProduct', async (req,res) => {
     try {
-        const newProduce = new ProductInfo({
-            productName: req.body.productName,
-            price: req.body.price,
-            shortDescription: req.body.shortDescription,
-            fullDescription: req.body.fullDescription,
-            quantity: req.body.quantity,
-            quantity_sale: req.body.quantity_sale,
-            type: req.body.type,
-            images: req.body.images,
-            rating: req.body.rating,
-            discount: req.body.discount,
-            addInfo: req.body.addInfo
-        });
+        const newProduce = new ProductInfo(req.body);
         const product = await newProduce.save();
         res.status(200).json(product);
     } catch (err) {
