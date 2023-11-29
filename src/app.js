fdbff50e0283f8142b14c0b516ce0eebad1ff872
nhinbm <mnhi0507@gmail.com>
2023-11-29 15:00:22 +0700

    temporary

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 //-----------------------------------
 const app = express();
 app.use(express.json());
 const corsOption = {
-    origin: "http://localhost:3000",
-    credentials: true
-}
-app.use(cors(corsOption));// sau này chỉnh lại thành đg dẫn mặc định
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+app.use(cors(corsOption)); // sau này chỉnh lại thành đg dẫn mặc định
 
 //-----------------------------------
 //routes & controller
-const productRoute = require('./routes/productRouter');
-const userRoute = require('./routes/userRouter');
+const productRoute = require("./routes/productRouter");
+const userRoute = require("./routes/userRouter");
 
 //-----------------------------------
 
-app.use('/product',productRoute);   
-app.use('/user',userRoute);
-app.use('/picture',express.static('public'));
+app.use("/product", productRoute);
+app.use("/user", userRoute);
+app.use("/picture", express.static("public"));
 // đường dẫn của 1 file ảnh là: /Livingroom/maimz_Sofa/img1.webp
 //-----------------------------------
 module.exports = app;
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,19 +1,65 @@
-const router = require('express').Router();
-const getAllProducts = require('../controller/productsController.js');
+const router = require("express").Router();
+const getAllProducts = require("../controller/productsController.js");
 
 //---------------------------------------------
 //CREATE - only for admin
-const ProductInfo = require('../models/productModel.js');
-router.post('/newProduct', async (req,res) => {
-    try {
-        const newProduce = new ProductInfo(req.body);
-        const product = await newProduce.save();
-        res.status(200).json(product);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+const ProductInfo = require("../models/productModel.js");
+router.post("/newProduct", async (req, res) => {
+  try {
+    const newProduce = new ProductInfo(req.body);
+    const product = await newProduce.save();
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-//GET ALL PRODUCT
-router.get('/',getAllProducts);
+// //GET ALL PRODUCT
+// router.get('/',getAllProducts);
+
+// Get all products
+router.get("/", async (req, res) => {
+  try {
+    // 1. Filtering
+    const queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    let query = ProductInfo.find(JSON.parse(queryStr));
+
+    // 2. Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // 3. Limiting the fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // 4. Pagination
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    console.log(page, limit, skip);
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const productCount = await ProductInfo.countDocuments();
+      if (skip >= productCount) throw new Error("This page does not exists");
+    }
+
+    const product = await query;
+    res.json(product);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 //-----------------------------------------------------
 module.exports = router;
