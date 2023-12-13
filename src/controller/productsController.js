23924b4da599de6c978b21f3630477f1afd97276
nhinbm <mnhi0507@gmail.com>
2023-12-13 23:48:32 +0700

    nhi temp

--- src/controller/productsController.js
+++ src/controller/productsController.js
@@ -1,78 +1,77 @@
-const ProductInfo = require('../models/Product');
+const ProductInfo = require("../models/Product");
 
-const getAllProducts = async (req,res, next) => {
-    try {
-        // 1. Filtering
-        const queryObj = { ...req.query };
-        const excludeFields = ["page", "sort", "limit", "fields"];
-        excludeFields.forEach((el) => delete queryObj[el]);
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-        let query = ProductInfo.find(JSON.parse(queryStr));
-    
-        // 2. Sorting
-        if (req.query.sort) {
-          const sortBy = req.query.sort.split(",").join(" ");
-          query = query.sort(sortBy);
-        } else {
-          query = query.sort("-createdAt");
-        }
-    
-        // 3. Limiting the fields
-        if (req.query.fields) {
-          const fields = req.query.fields.split(",").join(" ");
-          query = query.select(fields);
-        } else {
-          query = query.select("-__v");
-        }
-    
-        // 4. Pagination
-        const page = req.query.page;
-        const limit = req.query.limit;
-        const skip = (page - 1) * limit;
-        query = query.skip(skip).limit(limit);
-        if (req.query.page) {
-          const productCount = await ProductInfo.countDocuments();
-          if (skip >= productCount) throw new Error("This page does not exists");
-        }
-    
-        const product = await query;
-        res.json(product);
-      } catch (error) {
-        next(error);
-      }
-}
+const getAllProducts = async (req, res, next) => {
+  try {
+    // 1. Filtering
+    const queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    let query = ProductInfo.find(JSON.parse(queryStr));
+
+    // 2. Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // 3. Limiting the fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // 4. Pagination
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const productCount = await ProductInfo.countDocuments();
+      if (skip >= productCount) throw new Error("This page does not exists");
+    }
+
+    const product = await query;
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
+};
 
 //------------------------------for admin---------------------------------
 //add a product testing
-const addProduct = async (req,res, next) => {
+const addProduct = async (req, res, next) => {
   try {
-      
-      const newProduce = new ProductInfo(req.body);
-      const product = await newProduce.save();
-      res.status(201).json(product);
+    const newProduce = new ProductInfo(req.body);
+    const product = await newProduce.save();
+    res.status(201).json(product);
   } catch (err) {
-      next(err)
+    next(err);
   }
-}
+};
 // get list product
 const getAdminProduct = async (req, res, next) => {
   try {
     adminId = req.body._id;
-    listProduct = await ProductInfo.find({seller: adminId});
-    if(listProduct.length = 0){
+    listProduct = await ProductInfo.find({ seller: adminId });
+    if ((listProduct.length = 0)) {
       throw new Error("No product found");
     }
-  res.status(201).json(listProduct);
+    res.status(201).json(listProduct);
   } catch (error) {
     next(error);
   }
-}
-const updateProduct = async(req,res,next)=> {
+};
+const updateProduct = async (req, res, next) => {
   try {
-    const productId =  req.params.id;
+    const productId = req.params.id;
     const product = await ProductInfo.findById(productId);
-    if(!product){
+    if (!product) {
       throw new Error("This product does not exists");
     }
 
@@ -85,22 +84,18 @@ const updateProduct = async(req,res,next)=> {
     product.type = req.body.type || product.type;
     product.discount = req.body.discount || product.discount;
 
-    const updatePro = await product.save(); 
+    const updatePro = await product.save();
     res.status(201).json(updatePro.select("-images -seller"));
   } catch (err) {
     next(err);
   }
-}
-const deleteProduct = async(req,res,next)=> {
+};
+const deleteProduct = async (req, res, next) => {
   try {
-    const productId =  req.params.id;
-    const adminId = req.user._id;
+    const productId = req.params.id;
     const product = await ProductInfo.findById(productId);
-    if(!product){
+    if (!product) {
       throw new Error("This product does not exists");
-    }
-    if(product.seller === adminId) {
-      throw new Error("Not authorized, Can't delete");
     }
     const deletionResult = await ProductInfo.deleteOne({ _id: productId });
     if (deletionResult.deletedCount === 1) {
@@ -108,10 +103,15 @@ const deleteProduct = async(req,res,next)=> {
     } else {
       throw new Error("Failed to delete product");
     }
-
   } catch (err) {
     next(err);
   }
-}
+};
 
-module.exports = {getAllProducts, getAdminProduct,addProduct,updateProduct,deleteProduct};
+module.exports = {
+  getAllProducts,
+  getAdminProduct,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+};
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,71 +1,80 @@
-const {verify} = require('jsonwebtoken');
-const User = require('../models/User');
+const { verify } = require("jsonwebtoken");
+const User = require("../models/User");
 
 const authGuard = async (req, res, next) => {
-    try {
-        if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
-            const token = req.headers.authorization.split(" ")[1];
+  try {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      const token = req.headers.authorization.split(" ")[1];
 
-            if (!token) {
-                return res.status(401).json({ error: "Not authorized, No token" });
-            }
+      if (!token) {
+        return res.status(401).json({ error: "Not authorized, No token" });
+      }
 
-            try {
-                const { id } = verify(token, process.env.USER_TOKEN);
-                req.user = await User.findById(id).select("-password");
+      try {
+        const { id } = verify(token, process.env.USER_TOKEN);
+        req.user = await User.findById(id).select("-password");
 
-                if (!req.user) {
-                    return res.status(401).json({ error: "Not authorized, Token failed" });
-                }
-
-                next();
-            } catch (err) {
-                next(err);
-            }
-        } else {
-            return res.status(401).json({ error: "Not authorized, No token" });
+        if (!req.user) {
+          return res
+            .status(401)
+            .json({ error: "Not authorized, Token failed" });
         }
-    } catch (error) {
-        next(error);
+
+        next();
+      } catch (err) {
+        next(err);
+      }
+    } else {
+      return res.status(401).json({ error: "Not authorized, No token" });
     }
+  } catch (error) {
+    next(error);
+  }
 };
 
-
 const isAdmin = async (req, res, next) => {
-    try {
-        const seller = await User.findOne({email: req.body.email});
-        if(!seller) throw new Error("User not found!");
-        else if(seller.role != "admin") throw new Error("Not Admin, Login failed");
-        next();
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    const seller = await User.findOne({ email: req.body.email });
+    if (!seller) throw new Error("User not found!");
+    else if (seller.role != "admin") throw new Error("Not Admin, Login failed");
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
-const checkTokenAdmin = async (req,res,next)=>{
-    try {
-        if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
-            const token = req.headers.authorization.split(" ")[1];
-            if (!token) {
-                return res.status(401).json({ error: "Not authorized, No token" });
-            }
-            try {
-                const { id } = verify(token, process.env.SELLER_TOKEN);
-                req.user = await User.findById(id).select("_id userName");
+const checkTokenAdmin = async (req, res, next) => {
+  try {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      const token = req.headers.authorization.split(" ")[1];
+      if (!token) {
+        return res.status(401).json({ error: "Not authorized, No token" });
+      }
+      try {
+        const { id } = verify(token, process.env.SELLER_TOKEN);
+        req.user = await User.findById(id).select("_id userName");
 
-                if (!req.user) {
-                    return res.status(401).json({ error: "Not authorized, Token failed" });
-                }
-                next();
-            } catch (err) {
-                next(err);
-            }
-        } else {
-            return res.status(401).json({ error: "Not authorized, No token" });
+        if (!req.user) {
+          return res
+            .status(401)
+            .json({ error: "Not authorized, Token failed" });
         }
-    } catch (error) {
-        next(error);
+        next();
+      } catch (err) {
+        next(err);
+      }
+    } else {
+      return res.status(401).json({ error: "Not authorized, No token" });
     }
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports = {authGuard,isAdmin,checkTokenAdmin};
+module.exports = { authGuard, isAdmin, checkTokenAdmin };
